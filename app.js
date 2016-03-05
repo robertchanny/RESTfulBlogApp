edd24936a2ee763d9ca9350e26b35bfea325596e
Robert Chan <robertchanny@gmail.com>
2016-03-05 05:44:10 +0000

    Added edit and show

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var bodyParser = require("body-parser"),
-    mongoose   = require("mongoose"),
-    express    = require("express"),
-    app        = express();
+var bodyParser     = require("body-parser"),
+    mongoose       = require("mongoose"),
+    methodOverride = require("method-override"),
+    express        = require("express"),
+    app            = express();
     
 mongoose.connect("mongodb://localhost/restful_blog_app");    
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 var blogSchema = new mongoose.Schema({
    title: String,
@@ -46,6 +48,50 @@ app.post("/blogs", function(req,res) {
         } else {
             res.redirect("/blogs");
         }
+    });
+});
+
+//SHOW
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err,foundBlog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+//EDIT
+app.get('/blogs/:id/edit',function(req, res) {
+   Blog.findById(req.params.id, function(err, foundBlog) {
+       if(err) {
+            res.redirect('/blogs');
+       } else {
+            res.render('edit', {blog: foundBlog});
+       }
+   });
+});
+
+//UPDATE
+app.put('/blogs/:id', function(req,res) {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+       if(err) {
+           res.redirect('/blogs');
+       } else {
+           res.redirect('/blogs/' + req.params.id);
+       }
+    });
+});
+
+//DELETE
+app.delete("/blogs/:id", function(req,res) {
+    Blog.findByIdAndRemove(req.params.id, function(req,res) {
+       if(err) {
+           res.redirect('/blogs');
+       } else {
+           res.redirect('/blogs');
+       }
     });
 });
 
